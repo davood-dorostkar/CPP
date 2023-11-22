ec1fb495878a18b73d1978c676e1ca89391addfe
davood-dorostkar <dado2hacker@gmail.com>
2023-11-22 17:33:41 +0330

    add: course3 final project

--- Courses/Accelerated_CS/03_Unordered_DS/Week3,4-project/main.cpp
+++ Courses/Accelerated_CS/03_Unordered_DS/Week3,4-project/main.cpp
@@ -61,7 +61,7 @@ int main()
   srand(time(0));
 
   // Run the informal tests
-  // informalTests();
+  informalTests();
   // demoRunner();
 
   return 0;
@@ -92,7 +92,7 @@ void informalTests()
   // (The output will be very verbose.)
   constexpr bool doEx1Tests = false;
   constexpr bool doEx2Tests = false;
-  constexpr bool doEx3Tests = false;
+  constexpr bool doEx3Tests = true;
   // Some of the tests may take a few seconds to process. If that is annoying,
   // you can temporarily disable them by setting this to false:
   constexpr bool doEx3SlowTests = true; // requires doEx3Tests == true
